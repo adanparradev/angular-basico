220d2c97c8e982b2c22e929f3c41d1d051a4b0a6
Adan Parra <adanparrasocorro@gmail.com>
2022-10-11 10:27:29 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
-import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContadorComponent } from './contador/contador.component';
+
+import { HeroesModule } from './heroes/heroes.module';
+/*import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { ListadoComponent } from './heroes/listado/listado.component';*/
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponent
+    /*HeroeComponent,
+    ListadoComponent*/
+       
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
